335b503863b84c1f50da997c9152c06297322dde
mrbya <v.toth.svk2@gmail.com>
2026-03-19 10:21:58 +0100

    Fixed UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { isAuthenticated } from '$lib/stores/auth';
   import Layout from '$lib/components/Layout.svelte';
-  import Login from '../../views/Login.svelte';
+  import Login from '../views/Login.svelte';
 </script>
 
 {#if $isAuthenticated}
